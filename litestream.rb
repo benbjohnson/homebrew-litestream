924dd97ce55b490851df36eb8e7c9fe7cbcde192
Ben Johnson <benbjohnson@yahoo.com>
2022-03-11 09:01:29 -0700

    v0.3.8

--- litestream.rb
+++ litestream.rb
@@ -1,12 +1,12 @@
 class Litestream < Formula
   desc "Streaming replication for SQLite"
   homepage "https://github.com/benbjohnson/litestream"
-  version "0.3.7"
+  version "0.3.8"
   license "Apache-2.0"
 
   if OS.mac?
-    url "https://github.com/benbjohnson/litestream/releases/download/v0.3.7/litestream-v0.3.7-darwin-amd64.zip"
-    sha256 "fdfd811df081949fdac2f09af8ad624c37c02b98c0e777f725f69e67be270745"
+    url "https://github.com/benbjohnson/litestream/releases/download/v0.3.8/litestream-v0.3.8-darwin-amd64.zip"
+    sha256 "d359a4edd1cb98f59a1a7c787bbd0ed30c6cc3126b02deb05a0ca501ff94a46a"
   end
 
   def install
