ca40d7a9ba0dc33e7999f06ea849a6580fed3204
Ben Johnson <benbjohnson@yahoo.com>
2023-10-24 15:38:00 -0500

    v0.3.13

--- litestream.rb
+++ litestream.rb
@@ -6,12 +6,12 @@ class Litestream < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/benbjohnson/litestream/releases/download/v0.3.12/litestream-v0.3.12-darwin-amd64.zip"
-    sha256 "ec886c4a9c01cb888a199dab37b9c9abef544358e63cd60028067a1297d8d349"
+    sha256 "6f68536bd24a0b6c4fdefb2f91ba27332e2146fb9f1be7985adfee8cdfce9784"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/benbjohnson/litestream/releases/download/v0.3.12/litestream-v0.3.12-darwin-arm64.zip"
-    sha256 "1bdfd0aa7edca4acf8d66dffd869af2db087df5635d5d9a845ac5dfc4a548f6d"
+    sha256 "6d1689487432613f5c10aee75ee77c95250dcce4da49695bf0a448c794eb7daa"
   end
 
   def install
