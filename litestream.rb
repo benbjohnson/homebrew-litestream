00afaf7d0c1468f01e8dec2894ebe1cb7c9b0691
Ben Johnson <benbjohnson@yahoo.com>
2025-10-18 10:13:51 -0600

    v0.5.2

--- litestream.rb
+++ litestream.rb
@@ -1,17 +1,17 @@
 class Litestream < Formula
   desc "Streaming replication for SQLite"
   homepage "https://github.com/benbjohnson/litestream"
-  version "0.3.13"
+  version "0.5.2"
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/benbjohnson/litestream/releases/download/v0.3.13/litestream-v0.3.13-darwin-amd64.zip"
-    sha256 "6f68536bd24a0b6c4fdefb2f91ba27332e2146fb9f1be7985adfee8cdfce9784"
+    url "https://github.com/benbjohnson/litestream/releases/download/v0.5.2/litestream-0.5.2-darwin-x86_64.tar.gz"
+    sha256 "aeba26e911d9b0da79dbfd9d18c6753fff25568ac8adbc33d69b6025bc7f731c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/benbjohnson/litestream/releases/download/v0.3.13/litestream-v0.3.13-darwin-arm64.zip"
-    sha256 "6d1689487432613f5c10aee75ee77c95250dcce4da49695bf0a448c794eb7daa"
+    url "https://github.com/benbjohnson/litestream/releases/download/v0.5.2/litestream-0.5.2-darwin-arm64.tar.gz"
+    sha256 "c14830c969af47aafdb1bff681d01eb6c583d9edec01dfe9d88d797cf211ed30"
   end
 
   def install
